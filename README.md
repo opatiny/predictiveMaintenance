2dbe44cce36da60a55d5935b4549e538cf4a1e52
Oceane Patiny <oceane@patiny.com>
2025-03-03 14:43:04 +0100

    feat: plotSyîgnals works

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Predictive maintenance project
+
 ## Context
+
 Use warm-up data of micro5 milling machines in order to identify the current machine, and create a transformation matrix to adapt tools to the given machine.
 
 ## Objectives
 
+1. Cluster data in order to attribute new data to the correct machine
+2. Create a transformation matrix in order to normalize a machines data into a reference framework
--- src/plotSignals.py
+++ src/plotSignals.py
@@ -6,32 +6,41 @@ import pandas as pd
 
 
 # Function to plot signals
-def plotSignals(folderPath, savePath):
+def plotSignals(folderPath, savePath, nbPlots):
     # Create a new folder to save plots
     if not os.path.exists(savePath):
         os.makedirs(savePath)
 
     # Get all files in the folder
     files = os.listdir(folderPath)
-    files = sorted(files)[:3]
 
-    print(files)
+    # If nbPlots is None, plot all signals
+    if nbPlots is None:
+        nbPlots = len(files)
+
+    files = sorted(files)[:nbPlots]
+
+    print("Files to plot: ", files)
 
     # Loop through all files
     for file in files:
         # Read the file
-        data = pd.read_csv(folderPath + "/" + file, header=None)
+        data = pd.read_csv(
+            folderPath + "/" + file, header=None, sep=";", index_col=False
+        )
 
-        print(data)
+        # Convert time to seconds
+        data[0] = (data[0] - data[0][0]) / 1e9
 
         # Get the signal name
         signal_name = file.split(".")[0]
 
         # Plot the signal
         plt.figure()
-        plt.plot(data)
+        plt.plot(data[0], data[1], label=signal_name)
         plt.title(signal_name)
-        plt.xlabel("Time")
+        plt.xlabel("Time [s]")
         plt.ylabel("Value")
+        plt.grid()
         plt.savefig(savePath + "/" + signal_name + ".svg", format="svg")
         plt.close()
