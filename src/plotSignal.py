5c5528cd8cae81f0468790fb7535c118d3e31adc
Oceane Patiny <oceane@patiny.com>
2025-03-13 18:31:18 +0100

    feat(dataProcessing): create new notebook to test custom functions

--- src/plotSignal.py
+++ src/plotSignal.py
@@ -4,35 +4,49 @@ import math
 
 from utils import Utils
 
-folderPath = "data/Machine#1_Mecatis/Warmup_Mecatis_03_02_25/"
-filename = "stSigAxCurrentB.csv"
-filePath = folderPath + filename
 
-# Load data from csv file
-data = pd.read_csv(
-    filePath, sep=";", header=None
-)  # use names=["time", "value"] to add column names
+def plotSignal(filePath: str, nbPoints: int = None) -> None:
+    """
+    Plot a signal from a csv file.
 
-# sort data by time
-data = data.sort_values(by=0)
+    Parameters:
+    filePath (str): The path to the csv file containing the signal.
+    nbPoints (int, optional): The number of points to plot. If None, all points will be plotted.
 
-# convert time to seconds from beginning of array
-data.loc[:, 0] = Utils.getNormalizedTime(data.loc[:, 0])
+    Returns:
+    None
+    """
 
-# pick how many points to plot
-nbPoints = math.floor(data.shape[0] / 10)
-plotData = data.loc[:, :].head(nbPoints)
+    # Get filename
+    filename = filePath.split("/")[-1]
 
-# get y label
-yLabel = Utils.getYLabel(filename)
+    # Load data from csv file
+    data = pd.read_csv(
+        filePath, sep=";", header=None
+    )  # use names=["time", "value"] to add column names
 
-# Plot data
-plt.plot(plotData.loc[:, 0], plotData.loc[:, 1], "ro-", markersize=3)
-plt.title(filePath)
-plt.xlabel("Time [s]")
-plt.ylabel(yLabel)
-plt.grid(True)
-plt.show()
+    # sort data by time
+    data = data.sort_values(by=0)
 
-# Close all plots
-plt.close("all")
+    # convert time to seconds from beginning of array
+    data.loc[:, 0] = Utils.getNormalizedTime(data.loc[:, 0])
+
+    # pick how many points to plot
+    # If nbPlots is None, plot all signals
+    if nbPoints is None:
+        nbPoints = data.shape[0]
+    plotData = data.loc[:, :].head(nbPoints)
+
+    # get y label
+    yLabel = Utils.getYLabel(filename)
+
+    # Plot data
+    plt.plot(plotData.loc[:, 0], plotData.loc[:, 1], "ro-", markersize=3)
+    plt.title(filePath)
+    plt.xlabel("Time [s]")
+    plt.ylabel(yLabel)
+    plt.grid(True)
+    plt.show()
+
+    # Close all plots
+    plt.close("all")
