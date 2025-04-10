1067a59f1c8fe7591b646e0961e6f1a5a5a6190b
Oceane Patiny <oceane@patiny.com>
2025-04-10 12:11:43 +0200

    feat: compute slots for each segment

--- src/temperatureCorrection/getTemperatureCorrectionData.py
+++ src/temperatureCorrection/getTemperatureCorrectionData.py
@@ -5,14 +5,14 @@ from utils.computeSlotsAverage import computeSlotsAverage
 
 
 def getTemperatureCorrectionData(
-    samplePath: Path, timeSlot: float = 10, debug: bool = False
+    data: pd.DataFrame, timeSlot: float = 10, debug: bool = False
 ) -> pd.DataFrame:
     """
     Get the current and temperature data from a sample path.
 
     Parameters
     ----------
-    samplePath (str): The path to the sample.
+    sample (pd.DataFrame): The data to process.
     timeSlot (float): The duration of each slot on which to average in seconds.
         Defaults to 10 seconds.
     debug (bool, optional): Whether to print debug information. Defaults to True.
@@ -21,8 +21,6 @@ def getTemperatureCorrectionData(
     -------
     pd.DataFrame: The time, spindle current and spindle temperature data.
     """
-    # load the data
-    data = pd.read_parquet(samplePath)
     temperatureSlots = data[["timeSeconds", "lrSigSpindleTemp"]]
     currentSlots = data[["timeSeconds", "stSigAxCurrentS"]]
     if timeSlot != None:
