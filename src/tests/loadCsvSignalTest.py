57bee0a0f209ac8143142c78f9efce2ec910cb30
Oceane Patiny <oceane@patiny.com>
2025-04-14 13:47:41 +0200

    test: load csv signal test

--- src/tests/loadCsvSignalTest.py
+++ src/tests/loadCsvSignalTest.py
@@ -2,6 +2,8 @@ import os
 import sys
 from pathlib import Path
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 fileAbsPath = os.path.abspath(__file__)
 fileDir = os.path.dirname(fileAbsPath)
@@ -10,8 +12,7 @@ parentDir = os.path.dirname(fileDir)
 print("parentDir: ", parentDir)
 
 sys.path.append(parentDir)
-
-from plot.plotSignal import plotSignal
+from parse.loadCsvSignal import loadCsvSignal
 
 # get the current folder path
 currentPath = Path(__file__).resolve().parent
@@ -20,11 +21,11 @@ print(currentPath)
 relFolderPath = "../../data/Mecatis/03_02_25/"
 fileName = "lrSigSpindleTemp.csv"
 
-# concatenating the path
 filePath = str(currentPath / relFolderPath / fileName)
-print("filePath: ", filePath)
-plotSignal(filePath, nbPoints=None, debug=True)
-plt.show()
 
-# Close all plots
-plt.close("all")
+data = loadCsvSignal(filePath, debug=True)
+
+print("data: ", data)
+
+frequency = 1 / data["timeSeconds"].diff().mean()
+print("frequency: ", frequency)
